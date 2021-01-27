2c08c27a0bb92cabcf1f2cad49ca8e16696adc70
Davidtsiklauri <wiklauri.dato1997@gmail.com>
2021-01-27 16:14:21 +0400

    markup for week-selector

--- src/app/shared/components/week-selector/week-selector.component.ts
+++ src/app/shared/components/week-selector/week-selector.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./week-selector.component.scss']
 })
 export class WeekSelectorComponent implements OnInit {
-
+  weeks: any[] = [2, 3, 4, 5, 6, 78, 335, 34, 534, 5, 435, 435, 345, 34, 534, 5, 323, 232, 32, 32, 323];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  slide(idx: number) {
+    this.weeks.splice(idx, 1)
   }
 
 }
