4a963cfb7f793fedc108e4278f594098fdd6e620
Davidtsiklauri <wiklauri.dato1997@gmail.com>
2021-01-27 19:35:28 +0400

    started working on week-selector functionality

--- src/app/shared/components/week-selector/week-selector.component.ts
+++ src/app/shared/components/week-selector/week-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-week-selector',
@@ -7,13 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeekSelectorComponent implements OnInit {
   weeks: any[] = [2, 3, 4, 5, 6, 78, 335, 34, 534, 5, 435, 435, 345, 34, 534, 5, 323, 232, 32, 32, 323];
+  leftPosition: number = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  slide(idx: number) {
-    this.weeks.splice(idx, 1)
+  slide(idx: number, e: any) {
+    const centerIdx = 3;
+    const diff = idx - centerIdx;
+    this.leftPosition = this.leftPosition + (diff * 62);
   }
 
 }
